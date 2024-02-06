5c7881d747d767d86ff40dc3c9b5f7d5665125bb
Metin <metinnbolatt@gmail.com>
2024-02-06 22:18:29 +0300

    Final touches

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ require_once 'includes/session.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Task management with PHP, Bootstrap and Jquery">
     <title>Task Manager</title>
 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/custom.css">
     <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="assets/js/jquery-3.6.5.min.js"></script>
 
 </head>
 
