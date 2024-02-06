39a0d248751d41822d3314d74d577490af99d62c
Metin <metinnbolatt@gmail.com>
2024-02-06 22:13:36 +0300

    Added image upload modal

--- index.php
+++ index.php
@@ -44,7 +44,14 @@ require_once 'includes/session.php';
     <main class="flex-shrink-0">
 
         <div class="container  mt-5">
-            <h1>Tasks</h1>
+            <div class="row justify-content-start align-items-center">
+                <div class="col-4">
+                    <h1>Tasks</h1>
+                </div>
+                <div class="col-3">
+                    <button id="openModalButton" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#uploadImageModal">Upload Image</button>
+                </div>
+            </div>
             <table id="data-table" class="table table-bordered table-striped mt-5">
                 <div class="col-md-3 float-end">
                     <input id="search-input" type="text" class="form-control form-control-sm" placeholder="Search tasks...">
@@ -69,6 +76,25 @@ require_once 'includes/session.php';
             <span class="text-muted">Copyright &copy; 2024.</span>
         </div>
     </footer>
+    <div class="modal fade" id="uploadImageModal" tabindex="-1" aria-labelledby="uploadImageModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img class="img-thumbnail" id="selected-image" src="#" alt="Selected Image" style="display: none;">
+                    <input class="form-control mt-2" type="file" id="image-input" accept="image/*">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <script>
         // Fetch and load data on the table on page load
@@ -129,6 +155,14 @@ require_once 'includes/session.php';
                 $('#selected-image').attr('src', e.target.result).show();
             };
             reader.readAsDataURL(this.files[0]);
+        });
+
+        //Reset the image and the input when the modal is dismissed
+        $('#uploadImageModal').on('hidden.bs.modal', function(e) {
+            // Reset the image
+            $('#selected-image').attr('src', '').hide();
+            // Clear the file input
+            $('#image-input').val('');
         });
     </script>
 </body>
